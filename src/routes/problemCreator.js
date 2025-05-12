f1dc9e0931968376a7f4b5fad3e1ed1dfa009b00
AnshikaGoel <11402anshikagoyal@gmail.com>
2025-05-12 09:41:39 +0530

    problem

--- src/routes/problemCreator.js
+++ src/routes/problemCreator.js
@@ -4,15 +4,20 @@ const tokenVerifyMiddleware = require("../middleware/tokenverify")
 const {createProblem,updateProblem,deleteProblem,getProblembyId,getAllProblem} = require("../controllers/problemController")
 const adminmiddleware = require('../middleware/adminmiddleware')
 
+
+
+
 //these 3 need admin access
 problemRouter.post("/create", tokenVerifyMiddleware ,adminmiddleware, createProblem)
 problemRouter.put("/update/:id",tokenVerifyMiddleware,adminmiddleware,updateProblem)
 problemRouter.delete("/delete/:id",tokenVerifyMiddleware,adminmiddleware,deleteProblem)
 
 
-problemRouter.get("/:id",tokenVerifyMiddleware,getProblembyId)
-problemRouter.get("/",tokenVerifyMiddleware,getAllProblem)
-// problemRouter.post("/user",tokenVerifyMiddleware,solvedProblem)
+
+
+problemRouter.get("/problemById/:id",tokenVerifyMiddleware,getProblembyId)
+problemRouter.get("/getAllProblem",tokenVerifyMiddleware,getAllProblem)
+// problemRouter.post("/ProblemSolvedByUser",tokenVerifyMiddleware,solvedProblemByUser)
 // problemRouter.get("/filter",filterProblem)
 
 
